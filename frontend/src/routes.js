117642ee48a80af366a1900ab50b10c15be2e199
jorgeeder <jorgeeder.js@gmail.com>
2022-06-17 17:00:44 -0300

    feat: registration page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Logon from "./pages/Logon";
+import Register from "./pages/Register";
 
 export default function Ways() {
     return(
         <BrowserRouter>
             <Routes>
                 <Route  exact path="/" element={<Logon />} />
+                <Route  path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
