54ade6bbadcbb512fe8df039681a3a2aa55b4257
jorgeeder <jorgeeder.js@gmail.com>
2022-06-18 21:49:05 -0300

    feat: profile page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 export default function Ways() {
     return(
@@ -10,6 +11,7 @@ export default function Ways() {
             <Routes>
                 <Route  exact path="/" element={<Logon />} />
                 <Route  path="/register" element={<Register />} />
+                <Route  path="/profile" element={<Profile />} />
             </Routes>
         </BrowserRouter>
     );
