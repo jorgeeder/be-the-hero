43694e91d04e542b4665bc9f37bc106ce250846f
jorgeeder <jorgeeder.js@gmail.com>
2022-06-24 20:42:47 -0300

    feat: new incident page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import NewIncident from "./pages/NewIncident";
 
 export default function Ways() {
     return(
@@ -12,6 +13,7 @@ export default function Ways() {
                 <Route  exact path="/" element={<Logon />} />
                 <Route  path="/register" element={<Register />} />
                 <Route  path="/profile" element={<Profile />} />
+                <Route  path="/incidents/new" element={<NewIncident />} />
             </Routes>
         </BrowserRouter>
     );
