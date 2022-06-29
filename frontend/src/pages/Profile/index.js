a722eb6f92cc2c28c9492d016ed0be22b0d648a1
jorgeeder <jorgeeder.js@gmail.com>
2022-06-29 09:41:35 -0300

    feat: logout button navigation to index page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FiPower, FiTrash2 } from "react-icons/fi"
 
 import api from "../../services/api"
@@ -11,8 +11,11 @@ import logoImg from "../../assets/logo.svg";
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
 
+    const navigate = useNavigate();
+
     const ongId = localStorage.getItem("ongId");
     const ongName = localStorage.getItem("ongName");
+    
 
 
     useEffect(() => {
@@ -39,6 +42,12 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+
+        navigate("/");
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -46,7 +55,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
