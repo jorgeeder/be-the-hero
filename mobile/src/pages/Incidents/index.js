e369ea51120afa79c19d15d063525b9d4e5894e2
jorgeeder <jorgeeder.js@gmail.com>
2022-07-03 13:27:03 -0300

    feat: incident page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native"
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from "../../assets/logo.png";
 import styles from './styles';
 
 export default function Incidents() {
+    const navigation = useNavigation();
+
+    function navigationToDetail() {
+        navigation.navigate("Detail");
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -16,6 +24,34 @@ export default function Incidents() {
 
             <Text style={styles.title}>Bem-Vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+            <FlatList
+                data={[1, 2, 3]}
+                style={styles.incidentList}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$120,00</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={navigationToDetail}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name='arrow-right' size={16} color="#E02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+
 
         </View>
     );
